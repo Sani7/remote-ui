35545ef87332457464bc26d7140fbac528218758
Sander Speetjens <sander.speetjens@gmail.com>
2024-07-27 14:44:34 +0200

    Can debugger
    Add send can message

--- Qt/simulators/sim-can_debugger.h
+++ Qt/simulators/sim-can_debugger.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include "simulator_base.h"
+#include "hex_spinbox.h"
 
 namespace Ui {
 class Can_Debugger;
@@ -14,9 +15,11 @@ class Can_Debugger : public SimulatorBase {
   public:
     explicit Can_Debugger(Web_socket_wrapper* web_socket, QWidget *parent = nullptr);
     ~Can_Debugger();
+    void enable_can_send_data();
 
   private:
     Ui::Can_Debugger* ui;
+    std::vector<HexSpinBox*> can_send_hexspinboxes;
 };
 
 #endif // SIM_CAN_DEBUGGER_H
