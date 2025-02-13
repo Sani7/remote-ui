2e5ba3caae66f0faa954bb50f0e28e471fa7350f
Sander Speetjens <sander.speetjens@gmail.com>
2025-02-13 10:05:34 +0100

    update lint

--- lint.sh
+++ lint.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
-if (( $# < 1 )); then
-    >&2 echo "Illegal number of parameters"
-    exit 1
-fi
-
 # save unsaved work
 git stash --include-untracked
 
-clang-format -i "$@"
+find . -type f -regex '.*/.*\.\(c\|cpp\|h\|hpp\)$' -not -path "./external/*" -not -path "./build/*" -exec clang-format -i {} \;
 
 git add -A && git commit -m "Lint"
 
