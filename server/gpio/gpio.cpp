56727d253dcbe9ca9f662d2023903c4f665773f9
Sander Speetjens <sander.speetjens@gmail.com>
2025-04-14 08:53:06 +0200

    Lint

--- server/gpio/gpio.cpp
+++ server/gpio/gpio.cpp
@@ -24,30 +24,37 @@ void GPIO::configure_pin(Direction dir, uint8_t gpio)
     m_offset = gpio;
     if (dir == Direction::Output)
     {
-        try {
+        try
+        {
             m_request = std::make_unique<gpiod::line_request>(
                 ::gpiod::chip(chip_path)
                     .prepare_request()
                     .set_consumer("unisim_cpp")
-                    .add_line_settings(m_offset, ::gpiod::line_settings().set_direction(::gpiod::line::direction::OUTPUT))
+                    .add_line_settings(m_offset,
+                                       ::gpiod::line_settings().set_direction(::gpiod::line::direction::OUTPUT))
                     .do_request());
-        } catch (...) {
+        }
+        catch (...)
+        {
             SPDLOG_CRITICAL("Could not set gpio {} as output, run binary as root", gpio);
         }
     }
     if (dir == Direction::Input)
     {
-        try {
+        try
+        {
             m_request = std::make_unique<gpiod::line_request>(
                 ::gpiod::chip(chip_path)
                     .prepare_request()
                     .set_consumer("unisim_cpp")
-                    .add_line_settings(m_offset, ::gpiod::line_settings().set_direction(::gpiod::line::direction::INPUT))
+                    .add_line_settings(m_offset,
+                                       ::gpiod::line_settings().set_direction(::gpiod::line::direction::INPUT))
                     .do_request());
-        } catch (...) {
+        }
+        catch (...)
+        {
             SPDLOG_CRITICAL("Could not set gpio {} as input, run binary as root", gpio);
         }
-
     }
 }
 void GPIO::set_value(bool value)
