cb30ccc7f2d8d63c3ae9016fe7968bce08c588b5
Sander Speetjens <sander.speetjens@gmail.com>
2025-10-17 07:51:16 +0200

    Lint script
    Disable auto committing and make it optional via a cli flag

--- lint.sh
+++ lint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-# save unsaved work
-git stash --include-untracked
+for arg in "$@"; do
+  if [[ "$arg" == "-g" ]]; then
+    commit=true
+    # Put your command here
+  fi
+done
+
+if [ "$commit" == true ]; then
+    # save unsaved work
+    git stash --include-untracked
+fi
+
 
 find . -type f -regex '.*/.*\.\(c\|cpp\|h\|hpp\)$' -not -path "./external/*" -not -path "./build/*" -exec clang-format -i {} \;
 
-git add -A && git commit -m "Lint"
-
-git stash pop
+if [ "$commit" == true ]; then
+    git add -A && git commit -m "Lint"
+    git stash pop
+fi
